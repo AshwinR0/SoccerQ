cc30f33b614eedf4ec3ec8e657f0de3f6cd06379
AshwinR0 <ashwinrevi10@gmail.com>
2025-10-14 15:05:53 +0530

    Fixed logo spacing in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,10 +7,12 @@ const Footer = () => {
         <div>© {new Date().getFullYear()} SoccerQ — Built with ❤️</div>
         <div className="mt-1 flex items-center justify-center space-x-4">
           {/* <a href="ashwinrevi10gmail.com" className="flex items-center space-x-2 text-muted-foreground hover:text-foreground"> */}
+            <div className='flex items-center space-x-1 text-muted-foreground hover:text-foreground'>
             <Mail className="h-4 w-4" />
             <span className="text-sm">ashwinrevi10@gmail.com</span>
+            </div>
           {/* </a> */}
-          <a href="https://github.com/AshwinR0" target="_blank" rel="noreferrer" className="flex items-center space-x-2 text-muted-foreground hover:text-foreground">
+          <a href="https://github.com/AshwinR0" target="_blank" rel="noreferrer" className="flex items-center space-x-1 text-muted-foreground hover:text-foreground">
             <Github className="h-4 w-4" />
             <span className="text-sm">AshwinR0</span>
           </a>
